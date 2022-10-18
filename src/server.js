72a5aae543f2e9fe9c219d183948cf75c5d38206
gabrielasigolo <gabrielasigolo12@gmail.com>
2022-10-17 21:08:06 -0300

    second commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+const port = process.env.PORT || 3900
 
-app.use()
+app.use(cors())
+app.use(express.urlencoded({extended: true })) //permite acentos na URL
+app.use(express.json()) //Irá fazer o parse de arquivos JSON
+//Configura o favicon
+app.use('/favicon.ico', express.static('public/favicon.ico'))
+//Rotas de conteúdo público
+app.use('/', express.static('public'))
+
+
 
 app.get('/api', (req, res) => {
     res.status(200).json({
@@ -11,6 +21,18 @@ app.get('/api', (req, res) => {
     })
 })
 
-app.listen(4000, function() {
-    console.log('Servidor rodando!🚀')
+app.use(function(req, res){
+    res.status(404).json({
+        errors: [ 
+            {
+                value: `${req.originalUrl}`,
+                msg: `A rota ${req.originalUrl} não existe nessa API!`,
+                param: 'invalid route'
+            }
+        ]
+    })
+})
+
+app.listen(port, function() {
+    console.log(`Servidor rodando na porta ${port}!🚀`)
 })
